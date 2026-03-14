e7e90d3ddc224d8eff2ec6d47648540638ad9401
DaffaRamadhan1220 <daffasurya212@gmail.com>
2026-03-14 12:35:32 +0700

    Initial commit project modul3_NIM

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using JurnalModul4_103082400047;
+
+static void Main(string[] args)
+{
+    KodeBuah kodeBuah = new KodeBuah();
+
+    Console.WriteLine("=== Table-Driven (Kode Buah) ===");
+    Console.WriteLine("Kode buah untuk Apel: " + kodeBuah.getKodeBuah("Apel"));
+    Console.WriteLine("Kode buah untuk Pisang: " + kodeBuah.getKodeBuah("Pisang"));
+    Console.WriteLine("Kode buah untuk Durian: " + kodeBuah.getKodeBuah("Durian"));
+    Console.WriteLine("Kode buah untuk Semangka: " + kodeBuah.getKodeBuah("Semangka"));
+}
